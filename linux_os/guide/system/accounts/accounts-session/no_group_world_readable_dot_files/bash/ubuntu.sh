1aa6fe0cffefb47d207afabe367d7180a15eecf1
Alexander Scheel <alex.scheel@canonical.com>
2021-04-29 17:09:25 -0400

    Add remediation for no_group_world_readable_dot_files
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-session/no_group_world_readable_dot_files/bash/ubuntu.sh
@@ -0,0 +1,8 @@
+# platform = multi_platform_ubuntu
+
+. /usr/share/scap-security-guide/remediation_functions
+
+fetch_users_and_homedir |\
+while read user dir; do
+    find "$dir" -iname '.*' -type f -execdir chmod o-w,g-w {} \;
+done
