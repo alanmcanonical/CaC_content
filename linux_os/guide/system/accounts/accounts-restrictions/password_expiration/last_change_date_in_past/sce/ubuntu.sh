b5ee063e2ba3fea1cc6bc91d7ec4b7564e0e9fba
Alexander Scheel <alex.scheel@canonical.com>
2021-04-30 07:03:01 -0400

    Add SCE check for last_change_date_in_past
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/last_change_date_in_past/sce/ubuntu.sh
@@ -0,0 +1,21 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+#!/bin/bash
+#
+# "Copyright 2019 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+result=$XCCDF_RESULT_PASS
+
+# get todays date in epoch
+TZ=GMT
+today=$(($(date +%s) / 60 / 60 /24))
+cat /etc/shadow | awk -F: '{ print $1 " " $3 }' | while read user last_pwd_change; do
+	if [ "$last_pwd_change" -gt "$today" ]; then
+		echo "$user has a password change date in the future."
+		result=$XCCDF_RESULT_FAIL
+		break
+	fi
+done
+exit $result
