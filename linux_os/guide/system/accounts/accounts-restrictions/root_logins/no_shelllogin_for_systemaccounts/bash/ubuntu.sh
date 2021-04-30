05b6b823a36a48f89ed0d792d4b886ede2b560ce
Alexander Scheel <alex.scheel@canonical.com>
2021-04-30 08:23:07 -0400

    Add bash hardening for no_shelllogin_for_systemaccounts
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_shelllogin_for_systemaccounts/bash/ubuntu.sh
@@ -0,0 +1,14 @@
+# platform = multi_platform_ubuntu
+
+# set all system accounts to a non login shell
+awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false") {print $1}' /etc/passwd |\
+while read -r user; do
+    usermod -s "$(which nologin)" "$user";
+done
+
+# lock not root system accounts
+awk -F: '($1!="root" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"') {print $1}' /etc/passwd |\
+xargs -I '{}' passwd -S '{}' | awk '($2!="L" && $2!="LK") {print $1}' |\
+while read -r user; do
+    usermod -L "$user";
+done
