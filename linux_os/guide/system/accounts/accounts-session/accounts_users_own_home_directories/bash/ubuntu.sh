3cf19c2a62a638d2e5a5a2083b11b87a230e0877
Alexander Scheel <alex.scheel@canonical.com>
2021-04-30 07:05:21 -0400

    Add bash fix for accounts_users_own_home_directories
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-session/accounts_users_own_home_directories/bash/ubuntu.sh
@@ -0,0 +1,8 @@
+# platform = multi_platform_ubuntu
+
+. /usr/share/scap-security-guide/remediation_functions
+
+fetch_users_and_homedir |\
+while read user dir; do
+    chown "${user}" "${dir}"
+done
