6ff02eee0d33f45d540aadda639df8eb739b5e4b
Alexander Scheel <alex.scheel@canonical.com>
2021-05-10 08:54:45 -0400

    Check mount_has_to_exist in mount_options template
    Also fix bash.template for consistency, always checking the negative
    versus the positive.
    
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

--- shared/templates/mount_option/template.py
+++ shared/templates/mount_option/template.py
@@ -7,6 +7,8 @@ def _mount_option(data, lang):
         data["pointid"] = ssg.utils.escape_id(data["mountpoint"])
     else:
         data["mountoption"] = re.sub(" ", ",", data["mountoption"])
+    if 'mount_has_to_exist' not in data:
+        data['mount_has_to_exist'] = "no"
     return data
 
 
