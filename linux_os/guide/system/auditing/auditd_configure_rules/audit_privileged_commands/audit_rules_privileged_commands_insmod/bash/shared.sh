50cd2c5191bcde59687cf83da440653f1a592e76
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-05-13 11:05:40 -0300

    Add bash fix for auditing /sbin/insmod
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_insmod/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_insmod/bash/shared.sh
@@ -1,9 +1,11 @@
-# platform = multi_platform_sle,multi_platform_rhel
+# platform = multi_platform_sle,multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_ubuntu
+# reboot = false
+# complexity = low
+# disruption = low
 
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
 
-# Perform the remediation
 # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 fix_audit_watch_rule "auditctl" "/sbin/insmod" "x" "modules"
 fix_audit_watch_rule "augenrules" "/sbin/insmod" "x" "modules"
