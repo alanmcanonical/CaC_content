b97b068547105e426e050ecbf0a57ffa18ec9eeb
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-05-13 14:37:34 -0300

    Add bash fix for auditing /usr/bin/kmod
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_kmod/bash/shared.sh
@@ -0,0 +1,10 @@
+# platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_ubuntu
+# reboot = false
+# complexity = low
+# disruption = low
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+fix_audit_watch_rule "auditctl" "/usr/bin/kmod" "x" "modules"
+fix_audit_watch_rule "augenrules" "/usr/bin/kmod" "x" "modules"
