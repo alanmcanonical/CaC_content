eb574efb0b593aebd58aaa556aeb2c4aed34fadb
Alexander Scheel <alex.scheel@canonical.com>
2021-05-18 14:18:50 -0400

    Switch to non-interactive apt in ensure_apparmor_enforce_or_complain
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

--- linux_os/guide/system/apparmor/ensure_apparmor_enforce_or_complain/bash/ubuntu.sh
+++ linux_os/guide/system/apparmor/ensure_apparmor_enforce_or_complain/bash/ubuntu.sh
@@ -7,8 +7,8 @@
 local lvl1_aa_enforce="false"
 
 # apparmor and apparmor-utils must be installed to be compliant to this rule.
-{{{ bash_package_installed("apparmor") }}} || apt-get install apparmor -y
-{{{ bash_package_installed("apparmor-utils") }}} || apt-get install apparmor-utils -y
+{{{ bash_package_installed("apparmor") }}} || DEBIAN_FRONTEND=noninteractive apt-get install apparmor -y
+{{{ bash_package_installed("apparmor-utils") }}} || DEBIAN_FRONTEND=noninteractive apt-get install apparmor-utils -y
 
 if [ -z "${lvl1_aa_enforce}" ] || [ "${lvl1_aa_enforce}" != true ]; then
     aa-complain /etc/apparmor.d/*
