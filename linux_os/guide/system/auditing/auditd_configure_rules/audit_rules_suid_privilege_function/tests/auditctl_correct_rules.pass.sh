7f35e4fdf939a39118de853173b2344d1fe1034b
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-05-19 23:38:25 -0300

    Add tests for rule which audits suid/sgid privileged execution
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/tests/auditctl_correct_rules.pass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# use auditctl
+sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
+
+rm -rf /etc/audit/rules.d/*
+rm /etc/audit/audit.rules
+
+
+echo "-a always,exit -F arch=b32 -S execve -C uid!=euid -F euid=0 -k setuid" >> /etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S execve -C uid!=euid -F euid=0 -k setuid" >> /etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S execve -C gid!=egid -F egid=0 -k setgid" >> /etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S execve -C gid!=egid -F egid=0 -k setgid" >> /etc/audit/audit.rules
---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/tests/partial_rules.fail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "-a always,exit -F arch=b32 -S execve -C uid!=euid -F euid=0 -k setuid" >> /etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S execve -C uid!=euid -F euid=0 -k setuid" >> /etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S execve -C gid!=egid -F egid=0 -k setgid" >> /etc/audit/rules.d/setgid.rules
+echo "-a always,exit -F arch=b64 -S execve -C gid!=egid -F egid=0 -k setgid" >> /etc/audit/rules.d/setgid.rules
