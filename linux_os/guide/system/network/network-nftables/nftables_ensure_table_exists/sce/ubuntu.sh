cf11fe1cebb1666b8bbae7f89b12ba492ad445e1
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:10:57 -0400

    Add SCE for nftables_ensure_table_exists
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-nftables/nftables_ensure_table_exists/sce/ubuntu.sh
@@ -0,0 +1,24 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.2.4 Ensure a table exists
+rule_type="nftables"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+# Check if there are tables
+tbl_output=$(nft list tables)
+if [ -z "${tbl_output}" ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+exit ${XCCDF_RESULT_PASS}
