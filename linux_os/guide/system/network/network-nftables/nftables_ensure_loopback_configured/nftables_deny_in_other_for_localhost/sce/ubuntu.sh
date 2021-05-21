c1bfa3a5b709f99b6cfe650d196c1a70fb83bfa5
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:10:20 -0400

    Add SCE for nftables_deny_in_other_for_localhost
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-nftables/nftables_ensure_loopback_configured/nftables_deny_in_other_for_localhost/sce/ubuntu.sh
@@ -0,0 +1,23 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.2.6 Ensure loopback traffic is configured: nftables_deny_in_other_for_localhost
+rule_type="nftables"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+nft list ruleset | awk '/hook input/,/}/' | grep 'ip saddr'
+if [ $? -ne 0 ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+exit ${XCCDF_RESULT_PASS}
