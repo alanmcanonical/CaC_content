7eae2550355b251728c29ee912414ff53827b53d
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:11:18 -0400

    Add SCE for ufw_default_deny
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-ufw/ufw_default_deny/sce/ubuntu.sh
@@ -0,0 +1,25 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.1.7 Ensure default deny firewall policy
+rule_type="ufw"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+# if ufw is enabled, check for the default policy
+ufw status verbose |\
+    egrep -q "^Default:\s+deny\s+\(incoming\),\s+deny\s+\(outgoing\),\s+(disabled|deny)\s+\(routed\)"
+if [ $? -ne 0 ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+exit ${XCCDF_RESULT_PASS}
