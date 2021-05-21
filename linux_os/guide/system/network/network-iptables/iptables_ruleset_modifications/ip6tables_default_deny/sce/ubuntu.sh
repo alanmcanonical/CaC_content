3cf70e0cfcb4e8bb7a437c0d107db5212be89971
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:42:20 -0400

    Add SCE for ip6tables_default_deny
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-iptables/iptables_ruleset_modifications/ip6tables_default_deny/sce/ubuntu.sh
@@ -0,0 +1,41 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.3.2.1 Ensure default deny firewall policy
+rule_type="iptables"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+# Pass rule if IPv6 is disabled on kernel
+if [ ! -e /proc/sys/net/ipv6/conf/all/disable_ipv6 ] || [ "$(cat /proc/sys/net/ipv6/conf/all/disable_ipv6)" -eq 1 ]; then
+    exit $XCCDF_RESULT_PASS
+fi
+
+output="$(ip6tables -L | grep Chain)"
+if [ -z "${output}" ]; then
+        exit $XCCDF_RESULT_FAIL
+fi
+
+while read line; do
+
+	chain="$(echo $line | awk '{print $1, $2}')"
+	policy="$(echo $line | awk '{print $4}' | tr -d ")")"
+	if [ "$chain" = "Chain INPUT" ] || [ "$chain" = "Chain FORWARD" ] ||
+	   [ "$chain" = "Chain OUTPUT" ]; then
+		if [ "$policy" != "DROP" ] && [ "$policy" != "REJECT" ]; then
+			exit $XCCDF_RESULT_FAIL
+		fi
+	fi
+
+done <<< "$output"
+
+exit $XCCDF_RESULT_PASS
