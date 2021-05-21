70f21f7af6643c6c9346c50329af4ccc0f0abfae
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:09:55 -0400

    Add SCE for nftables_ensure_base_chain_exist
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-nftables/nftables_ensure_base_chain_exist/sce/ubuntu.sh
@@ -0,0 +1,26 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.2.5 Ensure base chains exist
+rule_type="nftables"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+# Check if there are base chains
+nft list ruleset | grep 'hook input' &>/dev/null &&\
+    nft list ruleset | grep 'hook forward' &>/dev/null &&\
+    nft list ruleset | grep 'hook output' &>/dev/null
+if [ $? -ne 0 ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+exit ${XCCDF_RESULT_PASS}
