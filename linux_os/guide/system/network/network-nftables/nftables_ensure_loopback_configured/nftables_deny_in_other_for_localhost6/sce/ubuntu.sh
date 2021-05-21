8d16e6bef136c56b25ba227e8e5df1b1556227c0
Alexander Scheel <alex.scheel@canonical.com>
2021-05-21 08:10:25 -0400

    Add SCE for nftables_deny_in_other_for_localhost6
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-nftables/nftables_ensure_loopback_configured/nftables_deny_in_other_for_localhost6/sce/ubuntu.sh
@@ -0,0 +1,25 @@
+##ssg## platform = multi_platform_ubuntu
+##ssg## check-import = stdout
+##ssg## check-export = var_firewall_package=fw_choice
+#!/bin/bash
+#
+# "Copyright 2021 Canonical Limited. All rights reserved."
+#
+#--------------------------------------------------------
+
+# 3.5.2.6 Ensure loopback traffic is configured: nftables_deny_in_other_for_localhost6
+rule_type="nftables"
+
+# verify if rule type matches the current firewall being audited
+if [ "${XCCDF_VALUE_fw_choice}" != "${rule_type}" ]; then
+    exit ${XCCDF_RESULT_NOT_APPLICABLE}
+fi
+
+if [ -e /proc/sys/net/ipv6/conf/all/disable_ipv6 ] && [ "$(cat /proc/sys/net/ipv6/conf/all/disable_ipv6)" -eq 0 ]; then
+    nft list ruleset | awk '/hook input/,/}/' | grep 'ip6 saddr'
+    if [ $? -ne 0 ]; then
+        exit ${XCCDF_RESULT_FAIL}
+    fi
+fi
+
+exit ${XCCDF_RESULT_PASS}
