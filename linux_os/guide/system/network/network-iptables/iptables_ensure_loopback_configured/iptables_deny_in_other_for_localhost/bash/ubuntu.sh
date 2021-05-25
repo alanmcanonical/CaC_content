1b3b4a0c98da1fc9cfaed74dc1d079bcafa0c484
Alexander Scheel <alex.scheel@canonical.com>
2021-05-25 09:50:54 -0400

    Add bash hardening for iptables_deny_in_other_for_localhost
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-iptables/iptables_ensure_loopback_configured/iptables_deny_in_other_for_localhost/bash/ubuntu.sh
@@ -0,0 +1,6 @@
+# platform = multi_platform_ubuntu
+
+{{{ bash_iptables_installed() }}}
+
+iptables -C INPUT -s 127.0.0.0/8 -j DROP 2>/dev/null || iptables -A INPUT -s 127.0.0.0/8 -j DROP
+iptables-save > /etc/iptables/rules.v4
