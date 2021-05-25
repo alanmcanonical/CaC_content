f7acb6a16dc6d22c448ab2941ba92de527dc5797
Alexander Scheel <alex.scheel@canonical.com>
2021-05-25 09:59:47 -0400

    Add bsah hardening for ip6tables_allow_out_lo
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/network/network-iptables/iptables_ensure_loopback_configured/ip6tables_allow_out_lo/bash/ubuntu.sh
@@ -0,0 +1,6 @@
+# platform = multi_platform_ubuntu
+
+{{{ bash_iptables_installed() }}}
+
+ip6tables -C OUTPUT -o lo -j ACCEPT 2>/dev/null || ip6tables -A OUTPUT -o lo -j ACCEPT
+ip6tables-save > /etc/iptables/rules.v6
