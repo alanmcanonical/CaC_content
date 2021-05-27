15a2bd70fb2bb5f99437d62b7054b3883ba451d9
Alexander Scheel <alex.scheel@canonical.com>
2021-05-27 09:52:31 -0400

    Add tests for file_owner_etc_issue_net
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-banners/file_owner_etc_issue_net/tests/correct.pass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+touch /etc/issue.net
+chown 0 /etc/issue.net
---
+++ linux_os/guide/system/accounts/accounts-banners/file_owner_etc_issue_net/tests/incorrect.fail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+touch /etc/issue.net
+chown 1 /etc/issue.net
