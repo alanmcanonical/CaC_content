8a1aa251257e8770b23d4a6f51f533b6dae9a3f5
Alexander Scheel <alex.scheel@canonical.com>
2021-05-27 09:29:52 -0400

    Add tests for no_etc_motd_leak
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-banners/no_etc_motd_leak/tests/constant.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+echo "This is my message of the day." > /etc/motd
---
+++ linux_os/guide/system/accounts/accounts-banners/no_etc_motd_leak/tests/embedded.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+echo "This is my motd for the host \v." > /etc/motd
