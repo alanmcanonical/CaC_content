b24138d0e591387516e93631f00e9401301f2e16
Alexander Scheel <alex.scheel@canonical.com>
2021-05-27 09:39:09 -0400

    Fix bash remediation for no_etc_issue_leak
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

--- linux_os/guide/system/accounts/accounts-banners/no_etc_issue_leak/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-banners/no_etc_issue_leak/bash/ubuntu.sh
@@ -1,5 +1,7 @@
 # platform = multi_platform_ubuntu
 
+. /usr/share/scap-security-guide/remediation_functions
+
 {{{ bash_instantiate_variables("login_banner_text") }}}
 
 echo "$login_banner_text" > /etc/issue
