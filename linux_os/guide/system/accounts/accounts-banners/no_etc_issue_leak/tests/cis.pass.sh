b17ef240565f2df2e1dd959da57a124a1dbf9107
Alexander Scheel <alex.scheel@canonical.com>
2021-05-27 09:39:18 -0400

    Add tests for no_etc_issue_leak
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-banners/no_etc_issue_leak/tests/cis.pass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = multi_platform_all
+# profiles = xccdf_org.ssgproject.content_profile_cis_level1_server,xccdf_org.ssgproject.content_profile_cis_level1_workstation,xccdf_org.ssgproject.content_profile_cis_level2_server,xccdf_org.ssgproject.content_profile_cis_level2_workstation
+
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue
---
+++ linux_os/guide/system/accounts/accounts-banners/no_etc_issue_leak/tests/embedded.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+echo "This is my issue for the host \v." > /etc/issue
