3b603483e9408cc2c4f9c39ef4a1418f4daa590d
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-06-01 18:22:57 +0200

    Add multi_platform_ubuntu to some bash scripts

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,10 +1,12 @@
-{{% if product in ["sle12", "sle15"] %}}
+# platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux,multi_platform_ubuntu
+
+. /usr/share/scap-security-guide/remediation_functions
+
+{{% if product in ["sle12", "sle15"] or 'ubuntu' in product %}}
 {{% set pam_lastlog_path = "/etc/pam.d/login" %}}
 {{% else %}}
 {{% set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
 {{% endif %}}
-# platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux
-. /usr/share/scap-security-guide/remediation_functions
 
 ensure_pam_module_options '{{{ pam_lastlog_path }}}' 'session' 'required' 'pam_lastlog.so' 'showfailed' "" ""
 
