8ce6499a7f2ed712bd37150e364e40d80340b823
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-06-01 17:07:20 +0200

    Add bash script to logs_permissions_var_log_audit

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/logs_permissions_var_log_audit/bash/shared.sh
@@ -0,0 +1,8 @@
+# platform = multi_platform_ubuntu
+
+if LC_ALL=C grep -iw log_file /etc/audit/auditd.conf; then
+    FILE=$(awk -F "=" '/log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+    chmod 0600 "$FILE"
+else
+    chmod 0600 /var/log/audit/audit.log*
+fi
