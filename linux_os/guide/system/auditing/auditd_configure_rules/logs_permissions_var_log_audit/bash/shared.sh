f5e260fb50b6bd99ffbc603cb3ce8fc275f36b7e
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-06-02 12:32:58 +0200

    Fix chmod command in logs_permissions_var_log_audit

--- linux_os/guide/system/auditing/auditd_configure_rules/logs_permissions_var_log_audit/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/logs_permissions_var_log_audit/bash/shared.sh
@@ -4,5 +4,5 @@ if LC_ALL=C grep -iw log_file /etc/audit/auditd.conf; then
     FILE=$(awk -F "=" '/log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
     chmod 0600 "$FILE"
 else
-    chmod 0600 /var/log/audit/audit.log*
+    chmod 0600 /var/log/audit/*
 fi
