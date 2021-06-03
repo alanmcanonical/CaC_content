fbd1137948596a5e97d6c2a9549eca2606eb4523
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-06-03 01:04:00 -0300

    Add tests for accounts_password_set_max_life_existing rule
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/user_maximum_age.fail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_cis_level1_server,xccdf_org.ssgproject.content_profile_cis_level1_workstation,xccdf_org.ssgproject.content_profile_cis_level2_server,xccdf_org.ssgproject.content_profile_cis_level2_workstation
+
+username=wichard
+
+sudo useradd ${username} -m --password '$6$fcj.zse3x/mbNHiF$pyR/mJuYIAf2LhguL7omzpgNKVqFqo322RBD5gC4k9h2UFSv7HC68iq7FgrAzAhS/aFJX4WvhzXBTobUw.j/w1' -s/bin/bash
+sudo chage --maxdays 99999 ${username}
---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/user_valid_age.pass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_cis_level1_server,xccdf_org.ssgproject.content_profile_cis_level1_workstation,xccdf_org.ssgproject.content_profile_cis_level2_server,xccdf_org.ssgproject.content_profile_cis_level2_workstation
+
+username=wichard
+
+sudo deluser ${username}
+sudo useradd ${username} -m --password '$6$fcj.zse3x/mbNHiF$pyR/mJuYIAf2LhguL7omzpgNKVqFqo322RBD5gC4k9h2UFSv7HC68iq7FgrAzAhS/aFJX4WvhzXBTobUw.j/w1' -s/bin/bash
+
+for usr in $(egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1); do
+    chage --maxdays 365 "${usr}"
+done
