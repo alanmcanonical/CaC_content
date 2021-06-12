c92f9b5588cc4728afabc73ebb42a0f43a59c10c
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-06-12 00:05:49 -0300

    Fix remediation script for accounts_passwords_pam_tally2
    - Remediation script now uses default value bases on variable
    - Use of unusual char on regexp for deny value, in order to force update
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2/bash/shared.sh
@@ -1,6 +1,8 @@
 # platform = multi_platform_sle
 . /usr/share/scap-security-guide/remediation_functions
 
-ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' 'required' 'pam_tally2.so' 'deny' '[123]' '3'
+{{{ bash_instantiate_variables("var_accounts_passwords_pam_tally2_deny") }}}
+# Use a non-number regexp to force update of the value of the deny option
+ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' 'required' 'pam_tally2.so' 'deny' '°' "${var_accounts_passwords_pam_tally2_deny}"
 ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' 'required' 'pam_tally2.so' 'onerr' '(fail)' 'fail'
 ensure_pam_module_options '/etc/pam.d/common-account' 'account' 'required' 'pam_tally2.so' '' '' ''
