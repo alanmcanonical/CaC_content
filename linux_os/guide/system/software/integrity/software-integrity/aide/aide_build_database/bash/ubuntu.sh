8ebbebc3ba9fed6ff48bce78b160fac9944efd10
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-06-23 23:05:45 -0300

    Enhancement to aide_build_database script
    - Script now fetches the information from aide config file
    - If the aide database path is not in the config file, it's added, since
      the OVAL relies on it (also valid for out database path).
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/bash/ubuntu.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/bash/ubuntu.sh
@@ -2,5 +2,22 @@
 
 {{{ bash_package_install("aide") }}}
 
-/usr/sbin/aideinit
-/bin/mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+AIDE_CONFIG=/etc/aide/aide.conf
+DEFAULT_DB_PATH=/var/lib/aide/aide.db
+
+# Fix db path in the config file, if necessary
+if ! grep -q '^database=file:' ${AIDE_CONFIG}; then
+    # replace_or_append gets confused by 'database=file' as a key, so should not be used.
+    #replace_or_append "${AIDE_CONFIG}" '^database=file' "${DEFAULT_DB_PATH}" '@CCENUM@' '%s:%s'
+    echo "database=file:${DEFAULT_DB_PATH}" >> ${AIDE_CONFIG}
+fi
+
+# Fix db out path in the config file, if necessary
+if ! grep -q '^database_out=file:' ${AIDE_CONFIG}; then
+    echo "database_out=file:${DEFAULT_DB_PATH}.new" >> ${AIDE_CONFIG}
+fi
+
+/usr/sbin/aideinit -y -f
