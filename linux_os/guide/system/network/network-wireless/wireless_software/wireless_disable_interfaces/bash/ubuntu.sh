40a7298f8591cb878e078bea70d6f8506609cd1f
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-06-29 15:19:40 +0200

    Add ubuntu specific bash for wireless_disable_interfaces

---
+++ linux_os/guide/system/network/network-wireless/wireless_software/wireless_disable_interfaces/bash/ubuntu.sh
@@ -0,0 +1,12 @@
+# platform = multi_platform_ubuntu
+
+if [ -n "$(find /sys/class/net/*/ -type d -name wireless)" ]; then
+    interfaces=$(ls -L -d /sys/class/net/*/wireless | xargs dirname | xargs basename)
+
+    for i in $interfaces; do
+        ifdown $i
+        drivers=$(basename $(readlink -f /sys/class/net/$i/device/driver))
+        echo "install $drivers /bin/true" >> /etc/modprobe.d/disable_wireless.conf
+        modprobe -r $drivers
+    done
+fi
