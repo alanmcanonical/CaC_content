1b9f8c7fc3cb9f7cf6bd2556a14f533e8b0f973d
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-07-13 18:45:20 +0200

    Add ubuntu specific bash to file_permissions_var_log

---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_permissions_var_log/bash/ubuntu.sh
@@ -0,0 +1,9 @@
+# platform = multi_platform_ubuntu
+
+. /usr/share/scap-security-guide/remediation_functions
+
+chmod 0755 /var/log/
+
+if grep -q "^z \/var\/log " /usr/lib/tmpfiles.d/00rsyslog.conf; then
+    sed -i --follow-symlinks "s/\(^z[[:space:]]\+\/var\/log[[:space:]]\+\)\(\([[:digit:]]\+\)[^ $]*\)/\10755/" /usr/lib/tmpfiles.d/00rsyslog.conf
+fi
