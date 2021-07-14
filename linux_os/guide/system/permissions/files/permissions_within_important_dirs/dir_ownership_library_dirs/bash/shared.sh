3f9ae1ea1b1097f2d4f8d0e08fd096573b6139b5
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-07-14 19:33:38 +0200

    Make dir_ownership_library_dirs use file_owner template

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/dir_ownership_library_dirs/bash/shared.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_ubuntu
-
-for LIBDIR in /usr/lib /usr/lib64 /lib /lib64
-do
-  if [ -d $LIBDIR ]
-  then
-    find -L $LIBDIR \! -user root -type d -exec chown root {} \;
-  fi
-done
