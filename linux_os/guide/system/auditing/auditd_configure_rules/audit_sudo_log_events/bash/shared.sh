cfd997c419a2a7107fa3d03673bc64a07ea2824a
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-07-15 13:47:43 +0200

    Make audit_sudo_log_events use audit_rule_login_events template

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_sudo_log_events/bash/shared.sh
+++
@@ -1,7 +0,0 @@
-# platform = multi_platform_ubuntu
-
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-
-fix_audit_watch_rule "auditctl" "/var/log/sudo.log" "wa" "maintenance"
-fix_audit_watch_rule "augenrules" "/var/log/sudo.log" "wa" "maintenance"
