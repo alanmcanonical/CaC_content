a2ac9051628ee519a5ce7625c32c9105c8b47c97
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-07-28 17:42:25 -0300

    Ubuntu-specific bash fix for dconf_gnome_banner_enabled
    Fix is applied on the /etc/gdm3/greeter.dconf-defaults file and the
    appropriated files inside /etc/dconf/db/gdm.d directory, in preparation
    for a future move to the only use the latter.
    
    Fix doesn't apply lock to the gdm option, since it isn't required by
    benchmarks
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_banner_enabled/bash/ubuntu.sh
@@ -0,0 +1,7 @@
+# platform = multi_platform_ubuntu
+
+# Will do both approach, since we plan to migrate to checks over dconf db. That way, future updates of the tool
+# will pass the check even if we decide to check only for the dconf db path.
+{{{ set_config_file("/etc/gdm3/greeter.dconf-defaults", "banner-message-enable", value="true", create='no', insert_after="\[org/gnome/login-screen\]", insert_before="", separator="=", separator_regex="", prefix_regex="^\s*") }}}
+{{{ bash_dconf_settings("org/gnome/login-screen", "banner-message-enable", "true", dconf_gdm_dir, "00-security-settings") }}}
+# No need to use dconf update, since bash_dconf_settings does that already
