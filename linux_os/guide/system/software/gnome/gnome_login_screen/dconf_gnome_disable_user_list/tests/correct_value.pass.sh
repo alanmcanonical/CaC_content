31ab6f55d728dd2b8b2a5cd48c1c89f056d575a3
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-08-02 15:14:31 -0300

    Ubuntu-specific tests for dconf_gnome_disable_user_list
    Test rules do the proper preconfiguration which consists of:
    - Update apt database
    - Install gdm3 instead of gdm and dconf
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_disable_user_list/tests/correct_value.pass.sh
+++ linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_disable_user_list/tests/correct_value.pass.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# packages = dconf,gdm
+# we don't use packages statement here since Ubuntu uses different packages from other distros.
 
 {{% set dconf_db = "distro.d" %}}
 {{% if product not in ("fedora", "rhel9") %}}
 {{% set dconf_db = "gdm.d" %}}
 {{% endif %}}
 
-. $SHARED/dconf_test_functions.sh
+{{% if 'ubuntu' not in product %}}
+    . $SHARED/dconf_test_functions.sh
 
-install_dconf_and_gdm_if_needed
+    install_dconf_and_gdm_if_needed
 
-clean_dconf_settings
-add_dconf_setting "org/gnome/login-screen" "disable-user-list" "true" "{{{ dconf_db }}}" "00-security-settings"
-add_dconf_lock "org/gnome/login-screen" "disable-user-list" "{{{ dconf_db }}}" "00-security-settings-lock"
+    clean_dconf_settings
+    add_dconf_setting "org/gnome/login-screen" "disable-user-list" "true" "{{{ dconf_db }}}" "00-security-settings"
+    add_dconf_lock "org/gnome/login-screen" "disable-user-list" "{{{ dconf_db }}}" "00-security-settings-lock"
+{{% else %}}
+    apt update
+    apt install gdm3 -y
+    conffile="/etc/gdm3/greeter.dconf-defaults"
+    sed -i '/disable-user-list=/d' ${conffile}
+    sed -i "/^\[org\/gnome\/login-screen\]/a""disable-user-list=true" ${conffile}
+{{% endif %}}
 
 dconf update
