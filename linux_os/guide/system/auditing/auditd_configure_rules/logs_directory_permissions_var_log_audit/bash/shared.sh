d8f8a7cbd80e73168cb2b5c87d628552bb6d261c
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-04 09:40:36 +0200

    Move to directory_permissions_var_log_audit

--- linux_os/guide/system/auditing/auditd_configure_rules/logs_directory_permissions_var_log_audit/bash/shared.sh
+++
@@ -1,8 +0,0 @@
-# platform = multi_platform_ubuntu
-
-if LC_ALL=C grep -iw ^log_file /etc/audit/auditd.conf; then
-     DIR=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ' | rev | cut -d"/" -f2- | rev)
-     chmod -R g-w,o-rwx "$DIR"
- else
-     chmod -R g-w,o-rwx /var/log/audit
- fi
