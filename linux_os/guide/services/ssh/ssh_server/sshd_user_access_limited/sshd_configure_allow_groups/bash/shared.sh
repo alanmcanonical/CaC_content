379a1b2f030180644b4bb8ef5b7dd52514145a3c
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-08-06 15:38:07 -0300

    Add bash fix for sshd_configure_allow_groups
    The magic number is used to check if the sshd config file will be
    changed or not. We don't add the magic number as a value to the
    AllowGroups parameter, because we don't want a default value added!
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_user_access_limited/sshd_configure_allow_groups/bash/shared.sh
@@ -0,0 +1,12 @@
+#platform = multi_platform_all
+
+. /usr/share/scap-security-guide/remediation_functions
+
+{{{ bash_instantiate_variables("var_sshd_allow_groups_valid") }}}
+
+# We don't add the default magic value to the sshd config file, since it's a security risk.
+default_value="e39d05b72f25767869d44391919434896bb055772d7969f74472032b03bc18418911f3b0e6dd47ff8f3b2323728225286c3cb36914d28dc7db40bdd786159c0a"
+
+if [ "${var_sshd_allow_groups_valid}" != ${default_value} ]; then
+    {{{ bash_sshd_config_set("AllowGroups", "$var_sshd_allow_groups_valid") }}}
+fi
