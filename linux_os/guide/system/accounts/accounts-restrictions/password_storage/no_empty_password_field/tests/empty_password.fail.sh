8692b46b719493787647ff06d2a3ad21df6ee2ce
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-08-09 17:07:19 -0300

    Add tests for rule no_empty_password_field
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_password_field/tests/empty_password.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "joas:x:1001:1001:,,,:/home/joas:/bin/bash" >> /etc/passwd
+echo "joas::18765:0:99999:7:::" >> /etc/shadow
---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_password_field/tests/no_empty_password.pass.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "joas:$6$gM5fpslHup7Ycjv0$nZxh4.SQalmatE43olfNXZUvzPLVDfhvT787QkiZ0aBggx.4Iw0WpqfKTB1ZTufliNWT1oJCIN7lURmKMs7q4/:18765:0:99999:7:::" >> /etc/shadow
