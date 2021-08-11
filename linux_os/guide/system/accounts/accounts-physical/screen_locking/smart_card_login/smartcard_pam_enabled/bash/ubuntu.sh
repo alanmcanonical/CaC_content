c24e3887e602bda9d9af3ebfb23a758fc07b02ee
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-11 16:33:17 +0200

    Source shared bash functions so fix works in ssg tests

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/bash/ubuntu.sh
@@ -1,3 +1,5 @@
 # platform = multi_platform_ubuntu
 
+. /usr/share/scap-security-guide/remediation_functions
+
 ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' '[success=2 default=ignore]' 'pam_pkcs11.so' '' '' ''
