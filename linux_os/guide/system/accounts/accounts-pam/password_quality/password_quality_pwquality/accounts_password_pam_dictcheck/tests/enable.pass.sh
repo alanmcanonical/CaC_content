785ad698c02006a3a0c0cf56aed46f9c8cd86589
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-12 23:01:54 +0200

    Fix tests for accounst_password_pam_dictcheck

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_dictcheck/tests/enable.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_dictcheck/tests/enable.pass.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+{{% if 'ubuntu' in product or 'debian' in product %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/common-password
+{{% else %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/system-auth
+{{% endif %}}
+
 echo "dictcheck=1" > /etc/security/pwquality.conf
--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_dictcheck/tests/not_defined.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_dictcheck/tests/not_defined.fail.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+{{% if 'ubuntu' in product or 'debian' in product %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/common-password
+{{% else %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/system-auth
+{{% endif %}}
+
 sed -i s/dictcheck.+//g /etc/security/pwquality.conf
