707814b90eac6a4884dd4249d0b92118c2fefb04
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-12 22:24:38 +0200

    Fix tests for accounts_password_pam_minlen

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_minlen/tests/commented.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_minlen/tests/commented.fail.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # packages = {{{- ssgts_package("pam_pwquality") -}}}
 
+{{% if 'ubuntu' in product or 'debian' in product %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/common-password
+{{% else %}}
+echo "password requisite pam_pwquality.so" > /etc/pam.d/system-auth
+{{% endif %}}
+
 CONF_FILE="/etc/security/pwquality.conf"
 
 if grep -q "^.*minlen\s*=" "$CONF_FILE"; then
