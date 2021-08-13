9185b5cda6b4c4f446a797a14453b7d560252ec5
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-13 10:02:39 +0200

    Change tests for accounts_password_pam_retry for multi platform

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/tests/argument_missing.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/tests/argument_missing.fail.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# platform = Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux
 
+{{% if 'ubuntu' not in product %}}
 config_file="/etc/pam.d/system-auth"
+{{% else %}}
+config_file="/etc/pam.d/common-password"
+{{% endif %}}
 
 if grep -q "pam_pwquality\.so.*retry=" "$config_file" ; then
 	sed -i --follow-symlinks "/pam_pwquality\.so/ s/\(retry *= *\).*//" $config_file
