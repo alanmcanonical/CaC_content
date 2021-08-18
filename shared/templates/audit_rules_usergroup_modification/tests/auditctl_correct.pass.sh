4df002638711ba409fda506e4b37e40e0d8377a3
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-18 11:00:42 +0200

    Add tests to template audit_rules_usergroup_modification

---
+++ shared/templates/audit_rules_usergroup_modification/tests/auditctl_correct.pass.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# packages = {{{ ssgts_package("audit") }}}
+
+echo "-w {{{ PATH }}} -p wa -k audit_rules_usergroup_modification" >> /etc/audit/audit.rules
+sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
---
+++ shared/templates/audit_rules_usergroup_modification/tests/augenrules_wrong_rule.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /etc/audit/rules.d
+echo "-w {{{ PATH }}} -p w -k audit_rules_usergroup_modification" >> /etc/audit/rules.d/login.rules
