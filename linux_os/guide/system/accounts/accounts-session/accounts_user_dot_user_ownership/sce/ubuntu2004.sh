d2fe3403185d24d0b879a1ccea15e7f02ddf245b
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-08-19 18:50:45 -0300

    Add SCE check for rule accounts_user_dot_user_ownership
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_user_ownership/sce/ubuntu2004.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Contributed by Canonical.
+#
+# Disable job control and run the last command of a pipeline in the current shell environment
+# Require Bash 4.2 and later
+#
+# platform = multi_platform_ubuntu
+# check-import = stdout
+
+set +m
+shopt -s lastpipe
+
+result=$XCCDF_RESULT_PASS
+
+cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/usr/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }'| while read user dir; do
+    if [ ! -d "$dir" ]; then
+        echo "The home directory ($dir) of user $user does not exist."
+    else
+        ulist=$(find ${dir} -maxdepth 1 -iname '.*' ! -user "${user}")
+        if [ -n "${ulist}" ]; then
+            result=$XCCDF_RESULT_FAIL
+            break
+        fi
+    fi
+done
+exit $result
