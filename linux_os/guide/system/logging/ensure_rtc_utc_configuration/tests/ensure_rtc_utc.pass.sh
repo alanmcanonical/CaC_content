e9a9c008ae86885d59140c1b1f17a016822809e4
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-24 11:19:07 +0200

    Make ensure_rtc_utc_configuration tests work in Ubuntu

---
+++ linux_os/guide/system/logging/ensure_rtc_utc_configuration/tests/ensure_rtc_utc.pass.sh
@@ -0,0 +1,3 @@
+# platform = SUSE Linux Enterprise 12,SUSE Linux Enterprise 15,multi_platform_ubuntu
+# packages = sudo
+sudo timedatectl set-timezone UTC
--- linux_os/guide/system/logging/ensure_rtc_utc_configuration/tests/ensure_rtc_utc_fail.sh
+++
@@ -1,3 +0,0 @@
-# platform = SUSE Linux Enterprise 12,SUSE Linux Enterprise 15
-# packages = sudo
-sudo timedatectl set-timezone CET
