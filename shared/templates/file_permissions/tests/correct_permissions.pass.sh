231ab38976fb2a558a7066a7437b8bffad468c02
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-25 11:17:28 +0200

    Add tests to template file_permissions

---
+++ shared/templates/file_permissions/tests/correct_permissions.pass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+{{% for path in FILEPATH %}}
+{{% if IS_DIRECTORY and FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex"
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    touch {{{ path }}}
+fi
+chmod {{{ FILEMODE }}} {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
