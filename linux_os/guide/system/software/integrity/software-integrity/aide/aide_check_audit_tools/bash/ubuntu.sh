3c96367003d01cf3a0cf2e801d59b68c1c6ec764
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-31 13:43:33 +0200

    Remove ubuntu specific bash

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_check_audit_tools/bash/ubuntu.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_ubuntu
-
-. /usr/share/scap-security-guide/remediation_functions
-
-if [ -f /etc/aide/aide.conf ]; then
-    for i in  'auditctl' 'auditd' 'ausearch' 'aureport' 'autrace' 'audispd' 'augenrules'; do
-        sed -i --follow-symlinks "/^\/sbin\/$i .*/{h;s/ .*/ p+i+n+u+g+s+b+acl+xattrs+sha512/};\${x;/^$/{s//\/sbin\/$i p+i+n+u+g+s+b+acl+xattrs+sha512/;H};x}" /etc/aide/aide.conf
-    done
-fi
