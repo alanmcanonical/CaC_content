aab7236cbe94397935b14e0ade6ae7b1785551e3
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-01 11:07:46 +0200

    Add tests to file_permissions_var_log_audit

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_permissions_var_log_audit/tests/incorrect_permissions.fail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# packages = {{{ ssgts_package("audit") }}}
+
+if LC_ALL=C grep -iw log_file /etc/audit/auditd.conf; then
+    FILE=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+else
+    FILE="/var/log/audit/audit.log"
+fi
+
+touch $FILE
+touch $FILE.1
+chmod 0777 $FILE
+chmod 0777 $FILE.*
