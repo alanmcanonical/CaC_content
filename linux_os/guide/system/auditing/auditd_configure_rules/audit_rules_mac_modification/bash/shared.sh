5c88c5bc406ec43ebcf47f7c951696aa46d8c6c6
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-09-02 16:16:30 -0300

    Fix audit_rules_mac_modification bash for Ubuntu
    - Ubuntu target uses AppArmor instead of SELinux
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_mac_modification/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_mac_modification/bash/shared.sh
@@ -1,7 +1,15 @@
-# platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel
+# platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_ubuntu
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
 
+
 # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
+{{% if 'ubuntu' not in product %}}
 fix_audit_watch_rule "auditctl" "/etc/selinux/" "wa" "MAC-policy"
 fix_audit_watch_rule "augenrules" "/etc/selinux/" "wa" "MAC-policy"
+{{% else %}}
+fix_audit_watch_rule "auditctl" "/etc/apparmor/" "wa" "MAC-policy"
+fix_audit_watch_rule "auditctl" "/etc/apparmor.d/" "wa" "MAC-policy"
+fix_audit_watch_rule "augenrules" "/etc/apparmor/" "wa" "MAC-policy"
+fix_audit_watch_rule "augenrules" "/etc/apparmor.d/" "wa" "MAC-policy"
+{{% endif %}}
