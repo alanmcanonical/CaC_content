a8f4a4d7720e0a10757a27f92f6cad82386b8c6e
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-03 16:57:11 +0200

    Add tests to apt_conf_disallow_unauthenticated

---
+++ linux_os/guide/services/apt/apt_conf_disallow_unauthenticated/tests/commented.pass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in /etc/apt/apt.conf.d/*; do
+    if [ -e "$file" ]; then
+        if grep -qi "APT::Get::AllowUnauthenticated" $file; then
+            sed -i 's/^.*APT::Get::AllowUnauthenticated.*/# APT::Get::AllowUnauthenticated.*/' $file
+        else
+            echo "# APT::Get::AllowUnauthenticated \"false\";" >> $file
+        fi
+    fi
+done
