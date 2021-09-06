a0bcceb273c1818f3e7d294b807fb6727b970119
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-06 23:04:41 +0200

    Remove ubuntu specific bash in favor of shared bash in display_login_attempts

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/ubuntu.sh
+++
@@ -1,4 +0,0 @@
-# platform = multi_platform_ubuntu
-. /usr/share/scap-security-guide/remediation_functions
-
-ensure_pam_module_options '/etc/pam.d/login' 'session' 'required' 'pam_lastlog.so' 'showfailed' '' '' 'silent'
