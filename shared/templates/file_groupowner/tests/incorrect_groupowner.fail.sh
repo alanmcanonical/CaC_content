19cb04f275cc991c706e59c3c8974de0be369926
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-08 18:18:46 +0200

    Add tests to template file_groupowner

---
+++ shared/templates/file_groupowner/tests/incorrect_groupowner.fail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+groupadd group_test
+
+{{% for path in FILEPATH %}}
+{{% if IS_DIRECTORY and FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex"
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    touch {{{ path }}}
+fi
+chgrp group_test {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
