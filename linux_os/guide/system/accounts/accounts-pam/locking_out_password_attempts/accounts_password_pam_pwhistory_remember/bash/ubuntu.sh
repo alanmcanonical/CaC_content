8235121b0bc0e4fae1e8c84b97c725a14964a51b
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-09-15 17:12:57 -0300

    Fix accounts_password_pam_pwhistory_remember
    - Script uses variable var_password_pam_remember to track the parameter
      of the 'remember' attribute for pam_pwhistory.
    - Fixed bad code from the script and enhanced the regexp to properly
      match some corner cases.
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
@@ -1,11 +1,13 @@
 # platform = multi_platform_ubuntu
+. /usr/share/scap-security-guide/remediation_functions
+{{{ bash_instantiate_variables("var_password_pam_remember") }}}
 
-local pamstr=`grep '^password required pam_pwhistory.so\b.*\bremember=' /etc/pam.d/common-password`
+pamstr=$(egrep '^password\s+required\s+pam_pwhistory\.so\b.*\bremember=' /etc/pam.d/common-password)
 if [ -z "$pamstr" ]; then
-    echo "password required pam_pwhistory.so remember=5" >> /etc/pam.d/common-password
+    echo "password required pam_pwhistory.so remember=${var_password_pam_remember}" >> /etc/pam.d/common-password
 else
-    local rememb_val=`echo -n "$pamstr" | egrep -o '\bremember=[0-9]+' | cut -d '=' -f 2`
-    if [ ${rememb_val} -lt 5 ]; then
-        sed -E -i "s/(^password required pam_pwhistory.so\b.*\bremember=)[0-9]+/\15/" /etc/pam.d/common-password
+    rememb_val=$(echo -n "$pamstr" | egrep -o '\bremember=[0-9]+' | cut -d '=' -f 2)
+    if [ -z "${rememb_val}" ] || [ ${rememb_val} -lt ${var_password_pam_remember} ]; then
+        sed -E -i "s/(^password\s+required\s+pam_pwhistory.so\b.*\bremember=)[0-9]*/\1${var_password_pam_remember}/" /etc/pam.d/common-password
     fi
 fi
