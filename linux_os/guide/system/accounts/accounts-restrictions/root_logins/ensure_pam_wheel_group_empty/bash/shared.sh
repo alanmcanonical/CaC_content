a4025f1705dd14b527fc638400bfaa88f3f3979d
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-09-16 20:08:21 -0300

    Add bash for ensure_pam_wheel_group_empty
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

---
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/ensure_pam_wheel_group_empty/bash/shared.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# platform = multi_platform_ubuntu
+. /usr/share/scap-security-guide/remediation_functions
+{{{ bash_instantiate_variables("var_pam_wheel_group_for_su") }}}
+
+GRP_FILE=/etc/group
+
+grep -q ^${var_pam_wheel_group_for_su}:[^:]*:[^:]*:[^:]* /etc/group
+if [ $? -ne 0 ]; then
+    groupadd ${var_pam_wheel_group_for_su}
+fi
+
+# group must be empty
+grp_memb=$(groupmems -g ${var_pam_wheel_group_for_su} -l)
+if [ -n "${grp_memb}" ]; then
+    for memb in ${grp_memb}; do
+        deluser ${memb} ${var_pam_wheel_group_for_su}
+    done
+fi
