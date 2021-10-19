bf6be7817f5cbc4320c2cf1342b378aeaa88ee95
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-10-19 17:21:02 +0200

    Remove auditd_audispd_configure_remote_server ubuntu specific bash

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_audispd_configure_remote_server/bash/ubuntu.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_ubuntu
-. /usr/share/scap-security-guide/remediation_functions
-{{{ bash_instantiate_variables("var_audispd_remote_server") }}}
-
-AUREMOTECONFIG=/etc/audisp/plugins.d/au-remote.conf
-AUDITCONFIG=/etc/audisp/audisp-remote.conf
-
-{{{ bash_replace_or_append("$AUREMOTECONFIG", '^active', 'yes', '@CCENUM@') }}}
-{{{ bash_replace_or_append("$AUDITCONFIG", '^remote_server', "$var_audispd_remote_server", "@CCENUM@") }}}
