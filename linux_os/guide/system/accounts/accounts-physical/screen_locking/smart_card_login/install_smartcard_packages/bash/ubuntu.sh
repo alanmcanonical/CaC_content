e9d8d611c582974c91c2fdf2bf6554c2e0c243fa
Eduardo Barretto <eduardo.barretto@canonical.com>
2022-10-18 16:26:09 +0200

    install_smartcard_packages: Add ubuntu specific remediation
    this avoids issues with pam module configuration.

---
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/install_smartcard_packages/bash/ubuntu.sh
@@ -0,0 +1,6 @@
+# platform = multi_platform_ubuntu
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y "libpam-pkcs11"
+
+cp /usr/share/doc/libpam-pkcs11/examples/pam_pkcs11.conf.example /etc/pam_pkcs11/pam_pkcs11.conf
+sed -i -e 's/debug = true/debug = false/g' -e 's|module = /usr/lib/opensc-pkcs11|module = /usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11|' /etc/pam_pkcs11/pam_pkcs11.conf
