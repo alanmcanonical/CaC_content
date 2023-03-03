d3bff76ccfd778704dcaf085703e7378ae427b25
David Fernandez Gonzalez <david.fernandezgonzalez@canonical.com>
2023-03-03 14:00:22 +0100

    Add module line before pam_unix.so

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
@@ -4,7 +4,7 @@
 
 pamstr=$(egrep '^password\s+required\s+pam_pwhistory\.so\b.*\bremember=' /etc/pam.d/common-password)
 if [ -z "$pamstr" ]; then
-    echo "password required pam_pwhistory.so remember=${var_password_pam_remember}" >> /etc/pam.d/common-password
+    sed -i "/pam_unix.so/ipassword    required            pam_pwhistory.so remember=${var_password_pam_remember}" /etc/pam.d/common-password
 else
     rememb_val=$(echo -n "$pamstr" | egrep -o '\bremember=[0-9]+' | cut -d '=' -f 2)
     if [ -z "${rememb_val}" ] || [ ${rememb_val} -lt ${var_password_pam_remember} ]; then
