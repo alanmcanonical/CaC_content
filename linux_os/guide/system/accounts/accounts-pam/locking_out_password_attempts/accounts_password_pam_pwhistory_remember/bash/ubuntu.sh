ad20ca60a56a33463c5bc170fc6002c7c296a0d1
David Fernandez Gonzalez <david.fernandezgonzalez@canonical.com>
2023-03-03 14:00:40 +0100

    Ensure use_authtok is correctly added

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember/bash/ubuntu.sh
@@ -11,3 +11,16 @@ else
         sed -E -i "s/(^password\s+required\s+pam_pwhistory.so\b.*\bremember=)[0-9]*/\1${var_password_pam_remember}/" /etc/pam.d/common-password
     fi
 fi
+
+# Check if we need to add use_authtok
+first_line=$(grep -n "^password" /etc/pam.d/common-password | cut -f1 -d: | head -n 1)
+pam_pwhistory_line=$(grep -n "pam_pwhistory.so" /etc/pam.d/common-password | cut -f1 -d: | head -n 1)
+if [ $first_line -eq $pam_pwhistory_line ]; then
+    if [[ ! $(egrep "pam_unix.so\b.*\buse_authtok" /etc/pam.d/common-password ) ]]; then
+        sed -i "s/pam_unix.so/& use_authtok/" /etc/pam.d/common-password
+    fi
+else
+    if [[ ! $(egrep "pam_pwhistory.so\b.*\buse_authtok" /etc/pam.d/common-password ) ]]; then
+        sed -E -i 's/pam_pwhistory.so/& use_authtok/' /etc/pam.d/common-password
+    fi
+fi
