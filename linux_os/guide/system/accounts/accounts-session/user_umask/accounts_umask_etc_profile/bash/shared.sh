8d63b901a2547a6642d15e3f3dc28167a41d18f8
Marcus Burghardt <maburgha@redhat.com>
2023-04-24 15:02:31 +0200

    Update Bash remediation to consider /etc/profile.d
    (cherry picked from commit 9dbf63b9981dd9a826c161aa68834d6bd46379e7)

--- linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_profile/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_profile/bash/shared.sh
@@ -1,9 +1,19 @@
 # platform = multi_platform_all
+# reboot = false
+# strategy = restrict
+# complexity = low
+# disruption = low
+
 . /usr/share/scap-security-guide/remediation_functions
+
 {{{ bash_instantiate_variables("var_accounts_user_umask") }}}
 
-grep -q umask /etc/profile && \
-  sed -i "s/umask.*/umask $var_accounts_user_umask/g" /etc/profile
-if ! [ $? -eq 0 ]; then
-    echo "umask $var_accounts_user_umask" >> /etc/profile
+readarray -t profile_files < <(find /etc/profile.d/ -type f -name '*.sh' -or -name 'sh.local')
+
+for file in "${profile_files[@]}" /etc/profile; do
+  grep -qE '^[^#]*umask' "$file" && sed -i "s/umask.*/umask $var_accounts_user_umask/g" "$file"
+done
+
+if ! grep -qrE '^[^#]*umask' /etc/profile*; then
+  echo "umask $var_accounts_user_umask" >> /etc/profile
 fi
