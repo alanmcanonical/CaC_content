ccc8d6beab6f3625020264aae09995bc6173ae33
Miha Purg <miha.purg@canonical.com>
2023-11-14 19:14:34 +0100

    Fix broken test in aide_build_database rule

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_not_present.fail.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_not_present.fail.sh
@@ -2,9 +2,9 @@
 # packages = aide
 
 if grep -qE 'DISTRIB_ID=Ubuntu' /etc/lsb-release; then
-    DB=/var/lib/aide/aide.db.gz
-else
     DB=/var/lib/aide/aide.db
+else
+    DB=/var/lib/aide/aide.db.gz
 fi
 
 rm -rf $DB
