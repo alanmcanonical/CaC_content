2f486ed4361adb80f4944ebae2bd059a91fa64d4
Miha Purg <miha.purg@canonical.com>
2023-12-04 15:54:42 +0100

    Fix backport by removing unused and incompatible jinja macros

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/bash/shared.sh
@@ -4,23 +4,6 @@
 # complexity = low
 # disruption = medium
 
-{{% if 'sle' in product %}}
-PAM_PATH="/etc/pam.d/"
-NULLOK_FILES=$(grep -rl ".*pam_unix\\.so.*nullok.*" ${PAM_PATH})
-for FILE in ${NULLOK_FILES}; do
-   sed --follow-symlinks -i 's/\<nullok\>//g' ${FILE}
-done
-{{% elif 'ubuntu' in product %}}
 for FILE in "/etc/pam.d/common-auth" "/etc/pam.d/common-password"; do
     sed -i 's/\(.*pam_unix\.so.*\)\s\<nullok\>\(.*\)/\1\2/g' ${FILE}
 done
-{{% else %}}
-if [ -f /usr/bin/authselect ]; then
-    {{{ bash_enable_authselect_feature('without-nullok') }}}
-else
-    {{{ bash_remove_pam_module_option('/etc/pam.d/system-auth', 'auth', 'sufficient', 'pam_unix.so', 'nullok') }}}
-    {{{ bash_remove_pam_module_option('/etc/pam.d/system-auth', 'password', 'sufficient', 'pam_unix.so', 'nullok') }}}
-    {{{ bash_remove_pam_module_option('/etc/pam.d/password-auth', 'auth', 'sufficient', 'pam_unix.so', 'nullok') }}}
-    {{{ bash_remove_pam_module_option('/etc/pam.d/password-auth', 'password', 'sufficient', 'pam_unix.so', 'nullok') }}}
-fi
-{{% endif %}}
