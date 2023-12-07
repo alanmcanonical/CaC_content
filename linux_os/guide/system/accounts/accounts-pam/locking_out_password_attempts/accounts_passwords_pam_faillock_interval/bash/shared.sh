d65299c3b17ca466a1ea27edef10a7dc58f53b23
Miha Purg <miha.purg@canonical.com>
2023-12-07 21:10:16 +0100

    Backport rule accounts_passwords_pam_faillock_interval from master
    Part of STIG v1r9 update

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
@@ -1,8 +1,6 @@
-# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv
-
-# include our remediation functions library
-. /usr/share/scap-security-guide/remediation_functions
+# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_ubuntu
 
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_fail_interval") }}}
 
-{{{ bash_set_faillock_option("fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
+{{{ bash_pam_faillock_enable() }}}
+{{{ bash_pam_faillock_parameter_value("fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
