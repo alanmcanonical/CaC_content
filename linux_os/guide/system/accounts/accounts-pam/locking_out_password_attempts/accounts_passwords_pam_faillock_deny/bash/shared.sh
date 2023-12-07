fad40989121515f896dfac83150aded114832dd6
Miha Purg <miha.purg@canonical.com>
2023-12-07 21:09:54 +0100

    Backport rule accounts_passwords_pam_faillock_deny from master
    Part of STIG v1r9 update

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
@@ -1,5 +1,6 @@
-# platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle
-. /usr/share/scap-security-guide/remediation_functions
+# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle,multi_platform_ubuntu
+
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_deny") }}}
 
-{{{ bash_set_faillock_option("deny", "$var_accounts_passwords_pam_faillock_deny") }}}
+{{{ bash_pam_faillock_enable() }}}
+{{{ bash_pam_faillock_parameter_value("deny", "$var_accounts_passwords_pam_faillock_deny") }}}
