5654b9c652e561d4fbfe5e9a16bef380cc9ada4b
Miha Purg <miha.purg@canonical.com>
2023-12-12 10:27:21 +0100

    Fix missing include in bash remediations

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
@@ -1,5 +1,7 @@
 # platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle,multi_platform_ubuntu
 
+. /usr/share/scap-security-guide/remediation_functions
+
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_deny") }}}
 
 {{{ bash_pam_faillock_enable() }}}
