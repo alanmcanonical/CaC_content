cf3f3714a978f381bb91bac09a998705481b294f
Miha Purg <miha.purg@canonical.com>
2023-12-14 20:53:00 +0100

    Update rule STIG-20-010124
    - Audit logs can no longer be owned by 'adm' group, only root
    - Fix OVAL, bash remediation and tests
    - Create a new CaC rule with suffix '_stig' to avoid conflicts with CIS 2.0.1.

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit_stig/bash/shared.sh
@@ -0,0 +1,10 @@
+# platform = multi_platform_ubuntu
+
+if LC_ALL=C grep -iw log_file /etc/audit/auditd.conf; then
+  FILE=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+else
+  FILE="/var/log/audit/audit.log"
+fi
+chgrp root $FILE
+
+{{{ bash_auditd_config_set("log_group", "root") }}}
---
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit_stig/tests/correct_value_default_file.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# packages = {{{ ssgts_package("audit") }}}
+
+source common.sh
+
+echo "log_group = root" >> /etc/audit/auditd.conf
+
+chgrp root ${FILE1}
