a8a5fff8b80f2e8deb5909d45a4164064c18199b
wqian <alan.moore@canonical.com>
2024-06-17 16:56:16 +0100

    Adopt the upstream oval check for dconf_gnome_banner_enable Source the necessary dconf_test_functions.sh

--- linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_banner_enabled/tests/correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_banner_enabled/tests/correct_value.pass.sh
@@ -2,6 +2,8 @@
 
 # we don't use packages statement here since Ubuntu uses different packages from other distros.
 
+source $SHARED/dconf_test_functions.sh
+
 install_dconf_and_gdm_if_needed
 clean_dconf_settings
 
--- linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_disable_user_list/tests/correct_value.pass.sh
+++ linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_disable_user_list/tests/correct_value.pass.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # we don't use packages statement here since Ubuntu uses different packages from other distros.
 
+source $SHARED/dconf_test_functions.sh
+
 install_dconf_and_gdm_if_needed
 clean_dconf_settings
 
