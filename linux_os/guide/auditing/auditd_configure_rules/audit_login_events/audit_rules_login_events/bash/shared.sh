7d69c9c78a69f8e3824c431f53265bf8ad00533e
wqian <alan.moore@canonical.com>
2024-07-22 19:04:33 +0100

    2

--- linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
@@ -1,7 +1,7 @@
 # platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel
 
 # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
-
+# Tri
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_dir") }}}
 
 {{{ bash_fix_audit_watch_rule("auditctl", "/var/log/tallylog", "wa", "logins") }}}
